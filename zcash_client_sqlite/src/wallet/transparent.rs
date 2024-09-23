1ef5803162e640fca99b27f5b1bc6e986b03f111
Willem Olding <willemolding@gmail.com>
2024-09-23 18:31:36 -0400

    fmt sqlite changes

--- zcash_client_sqlite/src/wallet/transparent.rs
+++ zcash_client_sqlite/src/wallet/transparent.rs
@@ -826,12 +826,16 @@ mod tests {
 
     #[test]
     fn put_received_transparent_utxo() {
-        zcash_client_backend::data_api::testing::transparent::put_received_transparent_utxo(TestDbFactory);
-
+        zcash_client_backend::data_api::testing::transparent::put_received_transparent_utxo(
+            TestDbFactory,
+        );
     }
 
     #[test]
     fn transparent_balance_across_shielding() {
-        zcash_client_backend::data_api::testing::transparent::transparent_balance_across_shielding(TestDbFactory, BlockCache::new());
+        zcash_client_backend::data_api::testing::transparent::transparent_balance_across_shielding(
+            TestDbFactory,
+            BlockCache::new(),
+        );
     }
 }
