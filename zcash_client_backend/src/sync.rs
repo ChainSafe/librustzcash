6e49c0d8f81057b1eb1250a8e7cf530dd9e716f5
Eric Tu <tu.eric@hotmail.com>
2024-09-26 15:51:42 -0400

    make all methods in sync public

--- zcash_client_backend/src/sync.rs
+++ zcash_client_backend/src/sync.rs
@@ -63,7 +63,7 @@ where
     P: Parameters + Send + 'static,
     ChT: GrpcService<BoxBody>,
     ChT::Error: Into<StdError>,
-    ChT::ResponseBody: Body<Data = Bytes> + Send + 'static,
+    ChT::ResponseBody: Body<Data=Bytes> + Send + 'static,
     <ChT::ResponseBody as Body>::Error: Into<StdError> + Send,
     CaT: BlockCache,
     CaT::Error: std::error::Error + Send + Sync + 'static,
@@ -91,13 +91,13 @@ where
         #[cfg(feature = "transparent-inputs")]
         wallet_birthday,
     )
-    .await?
+        .await?
     {}
 
     Ok(())
 }
 
-async fn running<P, ChT, CaT, DbT, TrErr>(
+pub async fn running<P, ChT, CaT, DbT, TrErr>(
     client: &mut CompactTxStreamerClient<ChT>,
     params: &P,
     db_cache: &CaT,
@@ -109,7 +109,7 @@ where
     P: Parameters + Send + 'static,
     ChT: GrpcService<BoxBody>,
     ChT::Error: Into<StdError>,
-    ChT::ResponseBody: Body<Data = Bytes> + Send + 'static,
+    ChT::ResponseBody: Body<Data=Bytes> + Send + 'static,
     <ChT::ResponseBody as Body>::Error: Into<StdError> + Send,
     CaT: BlockCache,
     CaT::Error: std::error::Error + Send + Sync + 'static,
@@ -238,14 +238,14 @@ where
     Ok(false)
 }
 
-async fn update_subtree_roots<ChT, DbT, CaErr, DbErr>(
+pub async fn update_subtree_roots<ChT, DbT, CaErr, DbErr>(
     client: &mut CompactTxStreamerClient<ChT>,
     db_data: &mut DbT,
 ) -> Result<(), Error<CaErr, DbErr, <DbT as WalletCommitmentTrees>::Error>>
 where
     ChT: GrpcService<BoxBody>,
     ChT::Error: Into<StdError>,
-    ChT::ResponseBody: Body<Data = Bytes> + Send + 'static,
+    ChT::ResponseBody: Body<Data=Bytes> + Send + 'static,
     <ChT::ResponseBody as Body>::Error: Into<StdError> + Send,
     DbT: WalletCommitmentTrees,
     <DbT as WalletCommitmentTrees>::Error: std::error::Error + Send + Sync + 'static,
@@ -299,14 +299,14 @@ where
     Ok(())
 }
 
-async fn update_chain_tip<ChT, DbT, CaErr, TrErr>(
+pub async fn update_chain_tip<ChT, DbT, CaErr, TrErr>(
     client: &mut CompactTxStreamerClient<ChT>,
     db_data: &mut DbT,
 ) -> Result<(), Error<CaErr, <DbT as WalletRead>::Error, TrErr>>
 where
     ChT: GrpcService<BoxBody>,
     ChT::Error: Into<StdError>,
-    ChT::ResponseBody: Body<Data = Bytes> + Send + 'static,
+    ChT::ResponseBody: Body<Data=Bytes> + Send + 'static,
     <ChT::ResponseBody as Body>::Error: Into<StdError> + Send,
     DbT: WalletWrite,
     DbT::Error: std::error::Error + Send + Sync + 'static,
@@ -327,7 +327,7 @@ where
     Ok(())
 }
 
-async fn download_blocks<ChT, CaT, DbErr, TrErr>(
+pub async fn download_blocks<ChT, CaT, DbErr, TrErr>(
     client: &mut CompactTxStreamerClient<ChT>,
     db_cache: &CaT,
     scan_range: &ScanRange,
@@ -335,7 +335,7 @@ async fn download_blocks<ChT, CaT, DbErr, TrErr>(
 where
     ChT: GrpcService<BoxBody>,
     ChT::Error: Into<StdError>,
-    ChT::ResponseBody: Body<Data = Bytes> + Send + 'static,
+    ChT::ResponseBody: Body<Data=Bytes> + Send + 'static,
     <ChT::ResponseBody as Body>::Error: Into<StdError> + Send,
     CaT: BlockCache,
     CaT::Error: std::error::Error + Send + Sync + 'static,
@@ -364,14 +364,14 @@ where
     Ok(())
 }
 
-async fn download_chain_state<ChT, CaErr, DbErr, TrErr>(
+pub async fn download_chain_state<ChT, CaErr, DbErr, TrErr>(
     client: &mut CompactTxStreamerClient<ChT>,
     block_height: BlockHeight,
 ) -> Result<ChainState, Error<CaErr, DbErr, TrErr>>
 where
     ChT: GrpcService<BoxBody>,
     ChT::Error: Into<StdError>,
-    ChT::ResponseBody: Body<Data = Bytes> + Send + 'static,
+    ChT::ResponseBody: Body<Data=Bytes> + Send + 'static,
     <ChT::ResponseBody as Body>::Error: Into<StdError> + Send,
 {
     let tree_state = client
@@ -391,7 +391,7 @@ where
 /// chain tip is out of sync with blockchain history.
 ///
 /// Returns `true` if scanning these blocks materially changed the suggested scan ranges.
-async fn scan_blocks<P, CaT, DbT, TrErr>(
+pub async fn scan_blocks<P, CaT, DbT, TrErr>(
     params: &P,
     db_cache: &CaT,
     db_data: &mut DbT,
@@ -488,7 +488,7 @@ where
 ///
 /// [a comment in the Android SDK]: https://github.com/Electric-Coin-Company/zcash-android-wallet-sdk/blob/855204fc8ae4057fdac939f98df4aa38c8e662f1/sdk-lib/src/main/java/cash/z/ecc/android/sdk/block/processor/CompactBlockProcessor.kt#L979-L991
 #[cfg(feature = "transparent-inputs")]
-async fn refresh_utxos<P, ChT, DbT, CaErr, TrErr>(
+pub async fn refresh_utxos<P, ChT, DbT, CaErr, TrErr>(
     params: &P,
     client: &mut CompactTxStreamerClient<ChT>,
     db_data: &mut DbT,
@@ -499,7 +499,7 @@ where
     P: Parameters + Send + 'static,
     ChT: GrpcService<BoxBody>,
     ChT::Error: Into<StdError>,
-    ChT::ResponseBody: Body<Data = Bytes> + Send + 'static,
+    ChT::ResponseBody: Body<Data=Bytes> + Send + 'static,
     <ChT::ResponseBody as Body>::Error: Into<StdError> + Send,
     DbT: WalletWrite,
     DbT::Error: std::error::Error + Send + Sync + 'static,
@@ -544,7 +544,7 @@ where
                             .map_err(|_| Error::MisbehavingServer)?,
                     ),
                 )
-                .ok_or(Error::MisbehavingServer)
+                    .ok_or(Error::MisbehavingServer)
             })
             .try_for_each(|output| {
                 let res = db_data.put_received_transparent_utxo(&output).map(|_| ());
@@ -605,8 +605,7 @@ where
     CaErr: std::error::Error,
     DbErr: std::error::Error,
     TrErr: std::error::Error,
-{
-}
+{}
 
 impl<CaErr, DbErr, TrErr> From<ChainError<DbErr, CaErr>> for Error<CaErr, DbErr, TrErr> {
     fn from(e: ChainError<DbErr, CaErr>) -> Self {
