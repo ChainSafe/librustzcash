5b94871da4d6f406b981c0cd221c8ed5c348839f
Willem Olding <willemolding@gmail.com>
2024-11-26 15:07:20 +1100

    add conversions for Note

--- zcash_client_memory/src/types/notes.rs
+++ zcash_client_memory/src/types/notes.rs
@@ -479,3 +479,115 @@ pub(crate) struct SentNote {
     pub(crate) value: Zatoshis,
     pub(crate) memo: Memo,
 }
+
+mod serialization {
+    use jubjub::Fr;
+
+    use super::*;
+    use crate::proto::memwallet as proto;
+
+    impl From<Note> for proto::Note {
+        fn from(note: Note) -> Self {
+            match note {
+                Note::Sapling(note) => Self {
+                    protocol: proto::ShieldedProtocol::Sapling.into(),
+                    recipient: note.recipient().to_bytes().to_vec(),
+                    value: note.value().inner(),
+                    rseed: match note.rseed() {
+                        sapling::Rseed::AfterZip212(inner) => Some(proto::RSeed {
+                            rseed_type: Some(proto::RSeedType::AfterZip212 as i32),
+                            payload: inner.to_vec(),
+                        }),
+                        sapling::Rseed::BeforeZip212(inner) => Some(proto::RSeed {
+                            rseed_type: Some(proto::RSeedType::BeforeZip212 as i32),
+                            payload: inner.to_bytes().to_vec(),
+                        }),
+                    },
+                    rho: None,
+                },
+                #[cfg(feature = "orchard")]
+                Note::Orchard(note) => Self {
+                    protocol: proto::ShieldedProtocol::Orchard.into(),
+                    recipient: note.recipient().to_raw_address_bytes().to_vec(),
+                    value: note.value().inner(),
+                    rseed: Some(proto::RSeed {
+                        rseed_type: None,
+                        payload: note.rseed().as_bytes().to_vec(),
+                    }),
+                    rho: Some(note.rho().to_bytes().to_vec()),
+                },
+            }
+        }
+    }
+
+    impl From<proto::Note> for Note {
+        fn from(note: proto::Note) -> Self {
+            match note.protocol {
+                0 => {
+                    let recipient =
+                        sapling::PaymentAddress::from_bytes(&note.recipient.try_into().unwrap())
+                            .unwrap();
+                    let value = sapling::value::NoteValue::from_raw(note.value);
+                    let rseed = match note.rseed {
+                        Some(proto::RSeed {
+                            rseed_type: Some(0),
+                            payload,
+                        }) => sapling::Rseed::BeforeZip212(
+                            Fr::from_bytes(&payload.try_into().unwrap()).unwrap(),
+                        ),
+                        Some(proto::RSeed {
+                            rseed_type: Some(1),
+                            payload,
+                        }) => sapling::Rseed::AfterZip212(payload.try_into().unwrap()),
+                        _ => panic!("rseed is required"),
+                    };
+                    Self::Sapling(sapling::Note::from_parts(recipient, value, rseed))
+                }
+                1 => {
+                    let recipient = orchard::Address::from_raw_address_bytes(
+                        &note.recipient.try_into().unwrap(),
+                    )
+                    .unwrap();
+                    let value = orchard::value::NoteValue::from_raw(note.value);
+                    let rho =
+                        orchard::note::Rho::from_bytes(&note.rho.unwrap().try_into().unwrap())
+                            .unwrap();
+                    let rseed = orchard::note::RandomSeed::from_bytes(
+                        note.rseed.unwrap().payload.try_into().unwrap(),
+                        &rho,
+                    )
+                    .unwrap();
+                    Self::Orchard(orchard::Note::from_parts(recipient, value, rho, rseed).unwrap())
+                }
+                _ => panic!("invalid protocol"),
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        use crate::proto::memwallet as proto;
+        use pretty_assertions::assert_eq;
+
+        #[test]
+        fn test_note_roundtrip() {
+            let note = Note::Sapling(sapling::note::Note::from_parts(
+                sapling::PaymentAddress::from_bytes(&[
+                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x30, 0x8e,
+                    0x11, 0x9d, 0x72, 0x99, 0x2b, 0x56, 0x0d, 0x26, 0x50, 0xff, 0xe0, 0xbe, 0x7f,
+                    0x35, 0x42, 0xfd, 0x97, 0x00, 0x3c, 0xb7, 0xcc, 0x3a, 0xbf, 0xf8, 0x1a, 0x7f,
+                    0x90, 0x37, 0xf3, 0xea,
+                ])
+                .unwrap(),
+                sapling::value::NoteValue::from_raw(99),
+                sapling::Rseed::AfterZip212([0; 32]),
+            ));
+
+            let proto_note: proto::Note = note.clone().into();
+            let recovered: Note = proto_note.into();
+
+            assert_eq!(note, recovered);
+        }
+    }
+}
